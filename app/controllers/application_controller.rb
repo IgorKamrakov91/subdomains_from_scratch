58694bef9fad0a53055928316a60c37165362a02
IgorKamrakov <i.kamrakov@yandex.ru>
2016-12-19 13:01:13 +0300

    Add subdomain and multi tenancy from scratch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_account
+
+  def require_account!
+    redirect_to root_url(subdomain: 'www') if !@account.present?
+  end
+
+  def set_account
+    @account = Account.find_by(subdomain: request.subdomain)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, account_attributes: [:subdomain]])
+  end
 end
